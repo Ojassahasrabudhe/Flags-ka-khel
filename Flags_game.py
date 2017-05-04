69d7569cea38f64cb4965be82c71ef961853d939
Ojas Sahasrabudhe <ojas.sahasrabudhe_ug19@ashoka.edu.in>
2017-05-04 19:24:59 +0530

    Added the option facility.
    As of now the code gives out options to the user. This will help reduce
    errors in the spelling. But the next step will be to give only four not
    ten options. And these four options will have to include the answer.
    
    The options are displayed once at the beginning. And then the code
    randomly chooses the which flags to display.

--- Flags_game.py
+++ Flags_game.py
@@ -1,5 +1,7 @@
 from PIL import Image
+import random 
 # I have imported Image library from PIL in onder to use images in my code. I have not completely understood what work a library does, but I read I need to do his on Stackexchange.
+# I have imported random to facilitate the random shuffle command which is used to give options randomly.
 
 group_of_countries = ['India','Bangladesh','Pakistan', 'Srilanka', 'Bermuda', 'Argentina', 'Brazil', 'Peru', 'Mexico', 'Canada' ]
 score = 0.0
@@ -7,12 +9,31 @@ score = 0.0
 #I have written score outside the for loop because we want the score to be added with every answer. I had previoulsy written the score in the for loop and this lead to score becoming zero with every new question.
 
 #I have created list of 10 countries for my game. The code lifts a country as per the written sequence, and then adds .jpg to the name of the element and picks up the image.
+print 'Your options are'
+print("\n".join(map(str, group_of_countries)))
+print random.shuffle(group_of_countries)
+
+#The above commands were written entirely by trial and error method. But they work!!
+#I am essentially printing out the entire list such that every element prints on a new line.
+# Then I shuffle the list and the program picks it up below! woah. I don't know how it decides which list to choose. The randomised one or the original one!?
+
 for questions in range(len(group_of_countries)):
 
 	flagImage = group_of_countries[questions].replace(' ', '+') + '.jpg'
 
 	image = Image.open(flagImage)
 	image.show()
+
+	#print ' Your options are'
+	#random.shuffle(group_of_countries)
+	#print (group_of_countries) 
+	#print("\n".join(map(str, group_of_countries)))
+
+	#Here I print'your options are' and in the next line I print the list of all the countries on the quiz in a random fasion.
+	# I need to figure out how to print only four random elements which also includes the answer element.
+	
+
+
 
 	answer = raw_input("\nWhich country's flag is this?\n")
 	#here if the answer matches the element of the list, then it will consider answer to be correct and add a point.
